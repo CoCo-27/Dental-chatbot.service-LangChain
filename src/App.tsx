5c940b63ac3da394f102feffabb427f0870e2324
CoCo-27 <cocokingdom27@gmail.com>
2023-06-26 08:25:13 -0400

    widget

--- src/App.tsx
+++ src/App.tsx
@@ -18,31 +18,40 @@ const App = () => {
 
   useEffect(() => {
     const updateDimension = () => {
-      setScreenSize(getCurrentDimension());
+      setScreenSize((prevSize) => ({ ...prevSize, ...getCurrentDimension() }));
     };
     window.addEventListener('resize', updateDimension);
-    if (window.location.pathname !== '/faq') {
-      const head = document.querySelector('head');
-      const script = document.createElement('script');
-      script.setAttribute('id', 'widgets');
+    return () => {
+      window.removeEventListener('resize', updateDimension);
+    };
+  }, []);
 
+  useEffect(() => {
+    const head = document.querySelector('body');
+    const script = document.createElement('script');
+    script.setAttribute('id', 'widgets');
+
+    if (window.location.pathname !== '/faq') {
       if (screenSize.width >= 1200) {
         script.setAttribute('src', '//localhost:8081/widget.js');
         head.appendChild(script);
       } else {
         const widgetsELem = document.getElementById('widgets');
-        if (widgetsELem !== null) widgetsELem.remove();
-      }
-      return () => {
-        if (screenSize.width >= 1200) {
-          head.removeChild(script);
+        const chatWidget = document.getElementById('jeeves-chat-widget');
+        if (widgetsELem !== null) {
+          widgetsELem.remove();
         }
-      };
+        if (chatWidget !== null) {
+          chatWidget.remove();
+        }
+      }
     }
     return () => {
-      window.removeEventListener('resize', updateDimension);
+      if (screenSize.width >= 1200) {
+        head.removeChild(script);
+      }
     };
-  }, [window.location.pathname, screenSize]);
+  }, [window.location.pathname, screenSize.width]);
 
   return (
     <div>
